5b75a990652fe6591e34be295d38e1b698837b5f
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-05 21:35:13 +0100

    adding vue

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+       # Will raise ActiveModel::ForbiddenAttributesError
+    @task = Task.new(user_params)
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:task).permit(:title, :details)
+  end
+end
